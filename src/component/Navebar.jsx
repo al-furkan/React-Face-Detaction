2863d0c81da705a01ad17f68ea2e98291299d0b3
furkan <furkan@gmail.com>
2024-03-09 21:08:11 +0600

    Your commit message

---
+++ src/component/Navebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navebar = () => {
+  return (
+    <div className="navbar">
+             <span>FonClick</span>
+        </div>
+  )
+}
+
+export default Navebar;
